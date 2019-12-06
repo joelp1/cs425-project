f42adb714d5dffb557e627b15bd3485427d93c70
nick <nicholastating@gmail.com>
2019-12-05 19:53:37 -0800

    changed something minor that I forgot what it was

--- customer.py
+++ customer.py
@@ -54,6 +54,7 @@ def add_address(newCountry, newState, city, zipCode, street, aNumber, unit = Non
 	csr = cnx.cursor()
 	csr.execute("INSERT INTO Address(country,aState,city,zipCode,street,aNumber,unit) VALUES (%(country)s,%(state)s,%(city)s,%(zip)s,%(street)s,%(num)s,%(unit)s)", d)
 	csr.execute("SELECT addressID FROM Address WHERE (country=%(country)s AND aState=%(state)s AND city=%(city)s AND zipCode=%(zip)s AND street=%(street)s AND aNumber=%(num)s AND unit=%(unit)s) LIMIT 1",d)
+	aid=0
 	for entry in csr:
 		aid = entry[0]
 	csr.execute("DELETE FROM Customer_Address WHERE customerID = %(cid)s", {"cid":login_customer_id})
